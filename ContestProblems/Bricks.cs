33ffd46f781cd5b6a04e4e313e549531f815f141
Safdhar10 <safdhard@gmail.com>
2021-02-19 23:54:48 +0530

    Contest Problem

---
+++ ContestProblems/Bricks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] get = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(get[0])/int.Parse(get[1]));
+        }
+    }
+}
