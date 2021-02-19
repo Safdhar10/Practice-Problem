43c22a89eea2ef5c16835de2765d8eeb280b1970
Safdhar10 <safdhard@gmail.com>
2021-02-19 23:53:20 +0530

    Contest Problem

---
+++ ContestProblems/BlackJack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJAck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] get = Console.ReadLine().Split();
+            if (int.Parse(get[0]) + int.Parse(get[1]) + int.Parse(get[2]) >= 22)
+            {
+                Console.WriteLine("bust");
+            }
+            else
+            {
+                Console.WriteLine("win");
+            }
+        }
+    }
+}
