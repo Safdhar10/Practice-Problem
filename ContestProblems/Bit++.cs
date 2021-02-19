4f31c75057fbdfa9c07cb61c9cfe3705606325a0
Safdhar10 <safdhard@gmail.com>
2021-02-19 23:50:51 +0530

    Contest Problem

---
+++ ContestProblems/Bit++.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int X = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                if (str == "++X")
+                {
+                    ++X;
+                }
+                else if (str == "--X")
+                {
+                    --X;
+                }
+                else if (str == "X++")
+                {
+                    X++;
+                }
+                else if (str == "X--")
+                {
+                    X--;
+                }
+            }
+            Console.WriteLine(X);
+        }
+    }
+}
