e2ae772f4571a91fc68d44334cdf3a24b51d7e31
Safdhar10 <safdhard@gmail.com>
2021-02-20 20:52:02 +0530

    Star Problem

---
+++ ContestProblems/Star.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerContest_Feb20_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            int R = X % 100;
+            Console.WriteLine(100-R);
+        }
+    }
+}
