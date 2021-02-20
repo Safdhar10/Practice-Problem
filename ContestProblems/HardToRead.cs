2c13402f650f94221de099620c7f561d80d522b7
Safdhar10 <safdhard@gmail.com>
2021-02-20 20:53:10 +0530

    Hard To Read Problem

---
+++ ContestProblems/HardToRead.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardToRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            bool check = true;
+
+            for(int k=0;k<S.Length;k++)
+            {
+                if(k%2!=0)
+                {
+                   if(S[k]>'A'||S[k]<'Z')
+                    {
+                        check = false;
+                        Console.WriteLine("No");
+                        return;
+
+                    }
+                }
+                else
+                {
+                    if(S[k]>'a' || S[k] < 'z')
+                    {
+                        check = false;
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("Yes");
+            //if(S.Length==1)
+            //{
+            //    if(char.IsLower(S[0]))
+            //    {
+            //        Console.WriteLine("Yes");
+            //        return;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("No");
+            //        return;
+            //    }
+            //}
+            //int i = 0;
+            //int j = 1;
+            //while (i<S.Length & j<S.Length)
+            //{
+            //   if(!( char.IsLower(S[i]) & char.IsUpper(S[j])))
+            //    {
+            //        Console.WriteLine("No");
+            //        return;
+            //    }
+            //    i = i + 2;
+            //    j = j + 2;
+            //}
+            //Console.WriteLine("Yes");
+        }
+    }
+}
