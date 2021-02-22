a5a8513e29a86fa3153810696e1325028f87fe12
Safdhar10 <safdhard@gmail.com>
2021-02-22 06:42:04 +0530

    Poor Problem

---
+++ ContestProblems/Poor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] get = Console.ReadLine().Split();
+            int a = int.Parse(get[0]);
+            int b = int.Parse(get[1]);
+            int c = int.Parse(get[2]);
+            int x = a ^ b ^ c;
+            if ((x==a) || (x==b) || (x==c))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
