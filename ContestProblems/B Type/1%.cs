33c0d631e92bc5042849f838e2dd4017113d0abf
Safdhar10 <safdhard@gmail.com>
2021-02-23 20:54:20 +0530

    One PErcent prblm

---
+++ ContestProblems/B Type/1%.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1yEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 X = Convert.ToInt64(Console.ReadLine());
+            Int64 n = 100;
+            int year = 0;
+            while(n<X)
+            {
+                n =n+(n/100);
+                year++;
+            }
+            Console.WriteLine(year);
+        }
+    }
+}
