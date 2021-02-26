614764d4c3121e7210b4b4d7d44099a16a9382eb
Safdhar10 <safdhard@gmail.com>
2021-02-26 07:49:51 +0530

    Contest prblm

---
+++ ContestProblems/WaterMelon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterMelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight =Convert.ToInt32(Console.ReadLine());
+            if (weight > 2 & weight % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
