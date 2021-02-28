9624c6854e7815dc42c445ee54824ecd5d1c4723
Safdhar10 <safdhard@gmail.com>
2021-02-28 20:52:21 +0530

    Online B Type Problem

---
+++ OnlineContest/B/PlaySnuke.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaySnuke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int min = int.MaxValue;
+            for(int i=0;i<n;i++)
+            {
+                int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int a = line[0];
+                int p = line[1];
+                int x = line[2];
+                if(a<x)
+                {
+                    if(min>p)
+                    {
+                        min = p;
+                    }
+                }
+            }
+            if(min==int.MaxValue)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(min);
+            }
+        }
+    }
+}
