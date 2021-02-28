e4f2e1179034f305ebbec39d360009d6b3215a53
Safdhar10 <safdhard@gmail.com>
2021-02-28 20:52:50 +0530

    Online C Type Problem

---
+++ OnlineContest/C/Unexpressed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unexpressed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = Convert.ToInt64(Console.ReadLine());
+            long x = 2;
+            long i = 2;
+            Dictionary<long,int> check = new Dictionary<long, int>();
+            while(i*i<=n)
+            {
+                long sum = i*i;
+                while(sum<=n)
+                {
+                    if(!check.ContainsKey(sum))
+                    {
+                        check.Add(sum, 1);
+
+                    }
+                    sum = sum * i;
+                }
+                i++;
+            }
+            Console.WriteLine(n-check.Count());
+        }
+    }
+}
