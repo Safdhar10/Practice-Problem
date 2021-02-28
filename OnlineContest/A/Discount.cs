1887a7ccaea328c442d801702cfa883b0279062c
Safdhar10 <safdhard@gmail.com>
2021-02-28 20:51:49 +0530

    Online A Type Problem

---
+++ OnlineContest/A/Discount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerContest_27Feb_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] line = Console.ReadLine().Split().Select(float.Parse).ToArray();
+            float A = line[0];
+            float B = line[1];
+            float check = A - B;
+            double result = (check / A)*100.00;
+            Console.WriteLine(result);
+        }
+    }
+}
