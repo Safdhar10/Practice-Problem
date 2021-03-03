64283b2ed0acd699be7d84f9cfc367ce7e321c9f
Safdhar10 <safdhard@gmail.com>
2021-03-03 16:21:52 +0530

    ArrayList

---
+++ DataStructure/DataStructure/ArrayList.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class ArrayList
+    {
+        int Capacity;
+        int[] data;
+        int Count;
+        public const int IsEmpty = 0;
+
+        public int Capacity1 { get => Capacity; set => Capacity = value; }
+        public int Count1 { get => Count; set => Count = value; }
+        public ArrayList()
+        {
+            Capacity1 = 2;
+            data = new int[Capacity1];
+        }
+
+        public void Print()
+        {
+            if (Count1!=IsEmpty)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("[");
+                for (int i = 0; i < Count1-1; i++)
+                {
+                    sb.Append(data[i] + ",");
+                }
+                sb.Append(data[Count1-1]+"]");
+                Console.WriteLine(sb.ToString()); 
+            }
+            else
+            {
+                Console.WriteLine("ArrayList IsEmpty"); 
+            }
+        }
+
+        public void Resize()
+        {
+            int[] oldArray = new int[Capacity1];
+            Array.Copy(data, oldArray, Capacity1);
+            //for(int i=0;i<Capacity1;i++)
+            //{
+            //    oldArray[i] = data[i];
+            //}
+            Capacity1 = (Capacity1 * 2);
+            data = new int[Capacity1];
+            //for(int j=0;j<oldArray.Length;j++)
+            //{
+            //    data[j] = oldArray[j];
+            //}
+            Array.Copy(oldArray, data, oldArray.Length);
+        }
+
+        public void Add(int value)
+        {
+            if (Count1 < Capacity1)
+            {
+                data[Count1] = value;
+                Count1++;
+            }
+            else
+            {
+                Resize();
+                Add(value);
+            }
+        }
+
+        public int Get(int index)
+        {
+            if (index < Count1 && index>=0)
+            {
+                return data[index];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void Set(int index, int value)
+        {
+                if (index < Count1 && index>=0)
+                {
+                    data[index] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Index was outside the bounds of the array ");
+                }
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (index <= Count1 && index>=0)
+            {
+                if(Count1==Capacity1)
+                {
+                    Resize();
+                }
+                for (int i = Count1; i > index; i--)
+                {
+                    data[i] = data[i - 1];
+                }
+                data[index] = value;
+                Count++;
+            }
+            else
+            {
+                Console.WriteLine("Index was outside the bound of the array");
+            }
+
+        }
+
+        public void Delete(int index)
+        {
+            if(Count1!=IsEmpty)
+            {
+                if (index >= 0 && index < Count1)
+                {
+                    for (int i = index; i <= Count1; i++)
+                    {
+                        if (Count1 != i)
+                        {
+                            data[i] = data[i + 1];
+                        }
+                        else
+                        {
+                            data[i] = 0;
+                        }
+                    }
+                    Count1--;
+                }
+                else
+                {
+                    Console.WriteLine("Index was outside the bounds of the array");
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            for (int i = 0; i < Count1; i++)
+            {
+                if (data[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Array List Count is {0}",Count1);
+        }
+
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            //When Array is Empty
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            al.Print();
+            Console.WriteLine("--------------------");
+            //Adding value using Add method
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Using Add Method");
+            al.Add(2);
+            al.Add(10);
+            al.Add(5);
+            al.Add(76);
+            Console.WriteLine();
+            al.Print();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            //Get Value By using get method
+            Console.WriteLine("Get Value of Index 2");
+            Console.WriteLine();
+            Console.WriteLine(al.Get(2));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------");
+
+            //trying to get outside of array list
+            Console.WriteLine("Trying to get value which is not exist");
+            Console.WriteLine();
+            Console.WriteLine(al.Get(-1));
+            Console.WriteLine(al.Get(4));//-1 is error message
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            //set value in existing array list values
+            Console.WriteLine("Set value 39 to Index 2");
+            Console.WriteLine();
+            al.Set(2, 39);
+            al.Print();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------");
+            //try to set value which is not existing in array list
+            Console.WriteLine("Trying to set which is not exist");
+            Console.WriteLine();
+            al.Set(6, 35);
+            al.Set(-2, 88);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            //insert value
+            Console.WriteLine("Insert value 25 in Index 1");
+            Console.WriteLine();
+            al.Insert(1, 25);
+            al.Print();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Try to insert value outside of the arraylist");
+            Console.WriteLine();
+            al.Insert(7, 99);
+            al.Insert(-1, 108);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Delete Value of Index 2");
+            al.Delete(2);
+            Console.WriteLine();
+            al.Print();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Trying To delete Which is not Existing");
+            Console.WriteLine();
+            al.Delete(-2);
+            al.Delete(8);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Checking value Contains or Not");
+            Console.WriteLine();
+            Console.WriteLine(al.Contains(39));
+            Console.WriteLine(al.Contains(89));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------");
+        }
+    }
+}
