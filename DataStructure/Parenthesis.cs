74ac19fe0e5b749932b77366c483f461d5a17c7e
Safdhar10 <safdhard@gmail.com>
2021-03-07 21:35:53 +0530

    Paranthesis.cs

---
+++ DataStructure/Parenthesis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParenthesisUsingStack
+{
+    public class Parenthesis
+    {
+        Stack<char> St = new Stack<char>();
+        public bool BalancedParanthesis(char[] input)
+        {
+            int _charLength = input.Length;
+
+            for (int i = 0; i < _charLength; i++)
+            {
+                if (input[i] == '(')
+                {
+                    St.Push(input[i]);
+                }
+                if (input[i] == ')')
+                {
+                    if (St.Count > 0)
+                    {
+                        if (St.Peek() == '(')
+                        {
+                            St.Pop();
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (St.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
