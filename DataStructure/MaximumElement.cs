a2accd9b2995f81c85d639ab57b293438bd685dc
Safdhar10 <safdhard@gmail.com>
2021-03-11 11:31:10 +0530

    Maximum Element

---
+++ DataStructure/MaximumElement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Stack<int> _s1 = new Stack<int>();
+            for(int i=1;i<=N;i++)
+            {
+                int[] _nextLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if(_nextLine[0]==2)
+                {
+                    _s1.Pop();
+                }
+                else if(_nextLine[0]==3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(_s1.Peek());
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    _s1.Push(_nextLine[1]);
+                }
+
+            }
+
+
+        }
+    }
+}
