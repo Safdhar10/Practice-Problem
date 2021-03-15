b069cb5539e4808b074d98785f68a3b7886795c4
Safdhar10 <safdhard@gmail.com>
2021-03-15 22:45:14 +0530

    Equal Stack

--- DataStructure/EqualStack.cs
+++ DataStructure/EqualStack.cs
@@ -18,24 +18,27 @@ namespace EqualStack
             Stack<int> _s2 = new Stack<int>();
             Stack<int> _s3 = new Stack<int>();
             int MaxLenth = Math.Max(_line2.Length, Math.Max(_line3.Length, _line4.Length));
+            int _s1Sum =0;
+            int _s2Sum = 0;
+            int _s3Sum =0;
             for (int i = MaxLenth - 1; i >= 0; i--)
             {
                 if (_line2.Length > i)
                 {
                     _s1.Push(Convert.ToInt32(_line2[i]));
+                    _s1Sum += _line2[i];
                 }
                 if (_line3.Length > i)
                 {
                     _s2.Push(Convert.ToInt32(_line3[i]));
+                    _s2Sum += _line3[i];
                 }
                 if (_line4.Length > i)
                 {
                     _s3.Push(Convert.ToInt32(_line4[i]));
+                    _s3Sum += _line4[i];
                 }
             }
-            int _s1Sum = _s1.Sum();
-            int _s2Sum = _s2.Sum();
-            int _s3Sum = _s3.Sum();
             while (!(_s1Sum == _s2Sum && _s2Sum == _s3Sum))
             {
                 if (_s1Sum > _s2Sum || _s1Sum > _s3Sum)
