aa4ed5e63de6aa6422c5983a60f7650623896032
Safdhar10 <safdhard@gmail.com>
2021-03-15 21:19:21 +0530

    Console App

---
+++ DataStructure/Queue/QueueConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quest_DataStructure;
+
+namespace Queue_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q1 = new Queue(5);
+            q1.EnQueue(10);
+        }
+    }
+}
