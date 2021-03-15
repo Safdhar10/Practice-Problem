c0bbbcce6c56464de046379f619ec031ac0b0a06
Safdhar10 <safdhard@gmail.com>
2021-03-15 21:22:51 +0530

    Orange

---
+++ ContestProblems/B Type/Orange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] _line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int A = _line[0];
+            int B = _line[1];
+            int C = _line[2] * 1000;
+            int _upper =C / A;
+            int _lower =(int) Math.Ceiling(C / (double)B);
+            if(_lower<=_upper)
+            {
+                Console.WriteLine(_lower + " "+ _upper);
+            }
+            else
+            {
+                Console.WriteLine("UNSATISFIABLE ");
+            }
+
+        }
+    }
+}
