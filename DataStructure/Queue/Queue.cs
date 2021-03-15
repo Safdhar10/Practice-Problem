090d3e27cb4cd8174b16cbc6934d4b19de544da3
Safdhar10 <safdhard@gmail.com>
2021-03-15 21:16:11 +0530

    Queue Data Structure

---
+++ DataStructure/Queue/Queue.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest_DataStructure
+{
+    public class Queue
+    {
+        int[] _data;
+        private int _count;
+        private int _capacity;
+        private bool _isEmpty;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                if (Count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private int _tail;
+        private int _head;
+        public Queue(int Capacity)
+        {
+            this._capacity = Capacity;
+            _data = new int[Capacity];
+            _count = 0;
+            _isEmpty = true;
+            _tail = -1;
+            _head = -1;
+        }
+        public void EnQueue(int value)
+        {
+            if (_count < _capacity)
+            {
+                if (_tail == (_capacity - 1))
+                {
+                    _tail = -1;
+                    EnQueue(value);
+                    _count--;
+                }
+                if (_count == 0)
+                {
+                    _head = 0;
+                }
+                _data[++_tail] = value;
+                _count++;
+            }
+            else
+            {
+                throw new OverflowException("Queue is Full");
+            }
+        }
+        public int DeQueue()
+        {
+            if(!IsEmpty)
+            {
+                if(_count>0)
+                {
+                    if(_head==(_capacity))
+                    {
+                        _head = 0;
+                        _count++;
+                    }
+                    _count--;
+                    return _data[_head++];
+                }
+                else
+                {
+                    throw new InvalidOperationException("Queue is empty");
+                }
+                
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+        }
+        public int Peek()
+        {
+            if(!IsEmpty)
+            {
+                return _data[_head];
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue is Empty");
+            }
+        }
+    }
+}
