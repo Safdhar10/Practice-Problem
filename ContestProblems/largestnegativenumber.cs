0939d28f261a2e4f50366753ec767750cc1d8d4d
Safdhar10 <safdhard@gmail.com>
2021-03-25 10:55:28 +0530

    largest modified

--- ContestProblems/largestnegativenumber.cs
+++ ContestProblems/largestnegativenumber.cs
@@ -10,21 +10,34 @@ namespace BattleshipConsole
     {
         static void Main(string[] args)
         {
+            int[] a1 = new int[] { 1, -2, -3, 5, -6, -8, 0, int.MinValue, 240, 50 };
+            int[] a2 = new int[] { 0, 1, 240, 50 };
+            int[] a3 = new int[] { int.MinValue, -4, -214748 };
+            int[] a4 = new int[] { -1, -2, int.MaxValue };
+            int[] a5 = new int[] { 1, 2, int.MinValue };
+            Console.WriteLine(largestNegative(a1));
+            Console.WriteLine(largestNegative(a2));
+            Console.WriteLine(largestNegative(a3));
+            Console.WriteLine(largestNegative(a4));
+            Console.WriteLine(largestNegative(a5));
+
+        }
+        public static int largestNegative(int[] array)
+        {
             int min = int.MinValue;
-            int[] array = new int[] { -1, -214748364, 3 };
             bool check = false;
-            for(int i=0;i<array.Length;i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                if(array[i]<0)
+                if (array[i] < 0)
                 {
+                    check = true;
                     if (array[i] > min)
                     {
                         min = array[i];
-                        check = true;
                     }
                 }
             }
-            Console.WriteLine(check?min:-1);
+            return check ? min : -1;
         }
     }
 }
