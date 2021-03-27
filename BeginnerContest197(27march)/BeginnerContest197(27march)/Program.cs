fca4bedbac59fefc201fd31aa42f13f9a12b1005
Safdhar10 <safdhard@gmail.com>
2021-03-27 18:35:13 +0530

    Rotate

---
+++ BeginnerContest197(27march)/BeginnerContest197(27march)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerContest197_27march_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            Console.WriteLine(S[1].ToString()+S[2].ToString()+S[0].ToString());
+        }
+    }
+}
