1a7eb98c4c8aa2a0ff8745c9ec01a28e65561969
Safdhar10 <safdhard@gmail.com>
2021-03-29 15:26:20 +0530

    String To Integer Problem

--- Challenge/StringToInteger/StringToInteger/Program.cs
+++ Challenge/StringToInteger/StringToInteger/Program.cs
@@ -10,6 +10,45 @@ namespace StringToInteger
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(WhiteSpace(" "));
         }
+        public static int WhiteSpace(string Number)
+        {
+            bool _digitCame = false;
+            int _answer = 0;
+            int _minusCount = 0;
+            if (Number == "2147483648" || Number.Equals("-2147483648"))
+            {
+                throw new ArithmeticException("Integer overflow has occurred");
+            }
+            for (int i = 0; i < Number.Length; i++)
+            {
+                if ((Number[i] >= 48 && Number[i] <= 57))
+                {
+                    int _digitValue = Number[i] - 48;
+                    _answer = (_answer * 10) + _digitValue;
+                    _digitCame = true;
+                }
+                else if(!_digitCame && Number[i]=='-')
+                {
+                    if (_minusCount < 1)
+                        _minusCount++;
+                    else
+                        throw new FormatException("Input string is not in the right format");
+
+                }
+                else if(_digitCame)
+                {
+                    if (Number[i] == '-')
+                        throw new FormatException("Input string is not in the right format");
+                    else
+                        break;
+                }
+            }
+            _answer= _minusCount == 1 ? (_answer * -1) : _answer;
+            return _digitCame?_answer:throw new FormatException("Input string is not in the right format");
+        }
+
+
     }
 }
--- Challenge/StringToInteger/StringToInteger/StringtoInt.cs
+++ Challenge/StringToInteger/StringToInteger/StringtoInt.cs
@@ -8,5 +8,18 @@ namespace StringToInteger
 {
     class StringtoInt
     {
+        public int ConvertoInt(string Number)
+        {
+            int _length = Number.Length;
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<Number.Length;i++)
+            {
+                if(Number[i]>='0' && Number[i]<='9')
+                {
+                    sb.Append(Number[i]);
+                }
+            }
+            return Convert.ToInt32(sb);
+        }
     }
 }
