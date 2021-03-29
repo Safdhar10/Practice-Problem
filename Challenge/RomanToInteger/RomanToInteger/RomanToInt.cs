c32f77dc7dbe8ce9cfea955d6c8375e772ba3f5e
Safdhar10 <safdhard@gmail.com>
2021-03-29 10:47:43 +0530

    sOME CHNAGES

--- Challenge/RomanToInteger/RomanToInteger/RomanToInt.cs
+++ Challenge/RomanToInteger/RomanToInteger/RomanToInt.cs
@@ -60,15 +60,5 @@ namespace RomanToInteger
                 return 1000;
             }
         }
-        public enum RomanValues
-        {
-            I=1,
-            V=5,
-            X=10,
-            L=50,
-            C=100,
-            D=500,
-            M=1000
-        }
     }
 }
