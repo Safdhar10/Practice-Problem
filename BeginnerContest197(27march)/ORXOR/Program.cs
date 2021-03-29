7f2bfe1a67d8a6a7ef7019f4eb038dd621829eeb
Safdhar10 <safdhard@gmail.com>
2021-03-29 10:19:58 +0530

    Challenge 3

---
+++ BeginnerContest197(27march)/ORXOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORXOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] _line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int _min = int.MaxValue;
+            int _first = (N / 2)-1;
+            int _S = 0;
+            int _F = 0;
+            for(int i=0;i<(_first+1);i++)
+            {
+                _S += (_line[i] | _line[i + 1]);
+                _F += (_line[i] ^ _line[i + 1]);
+            }
+            int _S1 = 0;
+            int _F1 = 0;
+            for(int j=(_first+1);j<N-1;j++)
+            {
+                _S1 += (_line[j] | _line[j + 1]);
+                _F1 += (_line[j] ^ _line[j + 1]);
+            }
+            Console.WriteLine(Math.Min(_S1,Math.Min(_S,Math.Min(_F,_F1))));
+        }
+    }
+}
---
+++ BeginnerContest197(27march)/Visiblity/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visiblity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int W = input[1];
+            int x = input[2];
+            int y = input[3];
+            int count = 0;
+            int count2 = 0;
+            for(int i=0;i<N;i++)
+            {
+                string line= Console.ReadLine();
+                if(i==x-1)
+                {
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        if (line[j]=='.')
+                        {
+                            count++;
+                        }
+                        else if(line[j]=='#' && count>0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if(i==x)
+                {
+                    for(int j=0;j<line.Length;j++)
+                    {
+                        if(line[j]=='.')
+                        {
+                            count2++;
+                        }
+                        else if(count2>0 && line[j]=='#')
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count+count2);
+        }
+    }
+}
