370088a3b4f238e5764dd0461232c4d5af11ef65
Safdhar10 <safdhard@gmail.com>
2021-04-10 21:38:06 +0530

    Spy CodeForce

---
+++ ContestProblems/SpyDetect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpyDetected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _t = Convert.ToInt32(Console.ReadLine());
+            for(int j=0;j<_t;j++)
+            {
+                int _n = Convert.ToInt32(Console.ReadLine());
+                int[] _a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int i = 0; i < _n-1; i++)
+                {
+                    if (_a[i]!=_a[i+1])
+                    {
+                        if(i==_n-2)
+                        {
+                            Console.WriteLine(i+2);
+                            break;
+                        }
+                        if (_a[i+1]!=_a[i+2])
+                        {
+                            Console.WriteLine(i+2);
+                            break;
+                        }
+                        Console.WriteLine(i+1);
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+}
